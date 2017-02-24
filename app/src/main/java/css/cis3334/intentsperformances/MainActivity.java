66a32c3355b2aedaadb84960567b41dd6789581a
ibishop <ibishop@css.edu>
2017-02-24 11:34:39 -0600

    In class participation 2.24.17

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +13,7 @@ import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1Web, but1Calendar, btn1Map;
+    Button btn1Web, but1Calendar, btn1Map, btn2Web, but2Calendar, btn2Map, btn3Web, but3Calendar, btn3Map;
     TextView textViewStatus;
 
     @Override
@@ -35,8 +36,12 @@ public class MainActivity extends AppCompatActivity {
         btn1Web = (Button) findViewById(R.id.button1WebInfo);
         btn1Web.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
-                textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d122420167%26view%3devent%26-childview%3d");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                textViewStatus.setText("Opening Webpage for performance 1");
             }
         });
 
@@ -53,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
                 calIntent.setType("vnd.android.cursor.item/event");
                 calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
                 calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
-                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
-                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 15, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 16, 40);
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
                         begDate.getTimeInMillis());
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
@@ -70,12 +75,126 @@ public class MainActivity extends AppCompatActivity {
         btn1Map = (Button) findViewById(R.id.button1map);
         btn1Map.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
                 textViewStatus.setText("Code should display map for performance 1");
             }
         });
 
+        //============================================================================================================
 
+        /**
+         *   Set up button click event listener for the web info button for the second performance
+         */
+        btn2Web = (Button) findViewById(R.id.button2WebInfo);
+        btn2Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://www.css.edu/About/Spotlight-Arts-and-Lectures/Calendar.html?trumbaEmbed=eventid%3D119983186%26view%3Devent%26-childview%3D");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                textViewStatus.setText("Opening Webpage for performance 2");
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the calendar button for the second performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but2Calendar = (Button) findViewById(R.id.button2Calendar);
+        but2Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 2");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "CSS Choirs at Home");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 3, 2, 19, 30);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 3, 2, 21, 30);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn2Map = (Button) findViewById(R.id.button2Map);
+        btn2Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                textViewStatus.setText("Code should display map for performance 2");
+            }
+        });
+
+        //============================================================================================================
+
+        /**
+         *   Set up button click event listener for the web info button for the third performance
+         */
+        btn3Web = (Button) findViewById(R.id.button3WebInfo);
+        btn3Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d119980577%26view%3devent%26-childview%3d");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                textViewStatus.setText("Opening Webpage for performance 3");
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the calendar button for the third performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but3Calendar = (Button) findViewById(R.id.button3Calendar);
+        but3Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 3");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "Twin Ports Choral Project");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 2, 19, 30);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 2, 21, 30);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn3Map = (Button) findViewById(R.id.button3Map);
+        btn3Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                textViewStatus.setText("Code should display map for performance 3");
+            }
+        });
     }
 
 
